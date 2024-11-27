7739e1bd7835780be78235878512fe2a0287886c
Petrus N Kirsten <petrus.kirsten@gmail.com>
2024-11-27 16:59:16 +0100

    Update SamplesFormulations.py

--- custom plots/SamplesFormulations.py
+++ custom plots/SamplesFormulations.py
@@ -25,80 +25,80 @@ def fonts(folder_path, small=10, medium=12):  # To config different fonts but it
     plt.rc('figure', titlesize=medium)  # fontsize of the figure title
 
 
-def plotFormulas(ax, formulas, title, typeCar=None, paneColor='whitesmoke', edgeColor='dimgrey'):
-    ax.set_proj_type('ortho')
-    ax.view_init(elev=29, azim=134, roll=0)
-    ax.grid(False)
-    for axis in ax.xaxis, ax.yaxis, ax.zaxis:
+def plotFormulas(
+        axes, formulas, title, typeCar=None,
+        markerColor='crimson', paneColor='whitesmoke', edgeColor='gainsboro'
+):
+    axes.set_proj_type('ortho')
+    axes.view_init(elev=16, azim=155, roll=0)
+    axes.grid(False)
+    for axis in axes.xaxis, axes.yaxis, axes.zaxis:
         axis.set_label_position('lower'), axis.set_ticks_position('lower')
         axis.set_pane_color(paneColor), axis.pane.set_edgecolor(edgeColor)
 
-    ax.set_title(title)
+    axes.set_title(title)
 
     # x axis
-    ax.set_xlabel('')
-    ax.set_xlim([3, -1])
-    ax.set_xticks([0, 1, 2])
-    ax.set_xticklabels(['No Car', 'kCar', 'iCar'])
+    axes.set_xlabel('')
+    axes.set_xlim([-0.5, 2.5])
+    axes.set_xticks([0, 1, 2])
+    axes.set_xticklabels(['No Car', 'kCar', 'iCar'])
     # ax.invert_xaxis()
 
     # y axis
-    ax.set_ylabel('Starch')
-    ax.set_ylim([12, -2] if typeCar else [2, 13])
-    ax.set_yticks([0, 5.0, 10.0] if typeCar else [5.0, 10.0])
-    ax.set_yticklabels(['0%', '5%', '10%'] if typeCar else ['5%', '10%'])
-    # ax.invert_yaxis()
+    axes.set_ylabel('Starch')
+    axes.set_ylim([-0.5, 2.5])
+    axes.set_yticks([0, 1, 2])
+    axes.set_yticklabels(['No St', '5%', '10%'])
+    axes.invert_yaxis()
 
     # z axis
-    ax.set_zlabel(f'Calcium ions concentration (mM)')
-    ax.set_zlim([0, 30])
-    ax.set_zticks([0, 7, 14, 28])
-    ax.set_zticklabels(['0', '7', '14', '28'])
+    axes.set_zlabel(f'Calcium ions concentration (mM)')
+    axes.set_zlim([-2, 45])
+    axes.set_zticks([0, 7, 14, 21, 28, 42])
+    axes.set_zticklabels(['0', '7', '14', '21', '28', '42'])
 
     for formulation in formulas:
         car, starch, cacl2 = formulation
-        color = 'whitesmoke'
-        markerFill = 'left'
-        ax.plot(car, starch, cacl2,
-                c=color, alpha=(0.4 + starch * 0.06), fillstyle=markerFill, ms=12, marker='o', mew=0.5, mec='k',
-                label='', zorder=2)
+        axes.plot(car, starch, cacl2,
+                  c=markerColor, alpha=1, ms=12, marker='o', mew=0.5, mec='k',
+                  label='', zorder=2)
 
-        ax.plot([car, car], [starch, starch], [0, cacl2 - 0.06],
-                color='#383838', linestyle='-', alpha=(0.4 + starch * 0.06), lw=0.6,
-                zorder=1)
+        axes.plot([car, car], [starch, starch], [0, cacl2 - 0.06],
+                  color='#383838', linestyle='-', alpha=0.8, lw=0.6,
+                  zorder=1)
 
 
-formulations = [
-    [0, 10, 0], [0, 10, 7], [0, 10, 14], [0, 10, 28],  # St
-    [1, 10, 0], [1, 10, 7], [1, 10, 14], [1, 10, 28],  # St + kappaCar
-    [2, 10, 0], [2, 10, 7], [2, 10, 14], [2, 10, 28],  # St + iotaCar
+finalForms = [
+    [0, 2, 0], [0, 2, 7], [0, 2, 14], [0, 2, 28],  # St
+    [1, 2, 0], [1, 2, 7], [1, 2, 14], [1, 2, 28],  # St + kappaCar
+    [2, 2, 0], [2, 2, 7], [2, 2, 14], [2, 2, 28],  # St + iotaCar
+]
+
+fivePct = [
+    [0, 1, 0],  # St
+    [1, 1, 0],  # St + kappaCar
+    [2, 1, 0],  # St + iotaCar
+]
+
+cars = [
+    [1, 0, 0], [1, 0, 7], [1, 0, 14], [1, 0, 21], [1, 0, 28], [1, 0, 42],  # St + kappaCar
+    [2, 0, 0], [2, 0, 7], [2, 0, 14], [2, 0, 21], [2, 0, 28], [2, 0, 42],  # St + iotaCar
 ]
 
 plt.style.use('seaborn-v0_8-ticks')
 fig = plt.figure(figsize=(8, 7), constrained_layout=False)
-fig.suptitle('Hydrogels formulations. 40 in total')
+fig.suptitle('')
 
-ax1 = fig.add_subplot(111, projection='3d')
-
-fig.subplots_adjust(
-    left=0.01, right=0.99,
-    top=0.87, bottom=0.02,
-    wspace=0.05, hspace=0.
-)
+ax = fig.add_subplot(111, projection='3d')
+fig.tight_layout()
 
 plotFormulas(
-    ax1, formulations,
-    'Starch-only | 12 formulations.',
-    paneColor='whitesmoke', edgeColor='gainsboro')
-
-# plotFormulas(
-#     ax2, kCar,
-#     'Kappa Carrageenan | 14 formulations.', 'Kappa Carrageenan',
-#     paneColor='floralwhite', edgeColor='antiquewhite')
-# plotFormulas(
-#     ax3, iCar,
-#     'Iota Carrageenan | 14 formulations.', 'Iota Carrageenan',
-#     paneColor='azure', edgeColor='paleturquoise')
+    ax, finalForms, '', markerColor='orange')
+plotFormulas(
+    ax, fivePct, '', markerColor='gainsboro')
+plotFormulas(
+    ax, cars, '',  markerColor='mediumspringgreen')
 
 filename = 'Hydrogels-Formulations-Plot-NEW'
 plt.show()
